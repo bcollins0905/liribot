21af81d998458fe02ffaad9999b03be4ec858713
bcollins0905 <b.w.collinssf@gmail.com>
2017-10-21 00:08:47 -0700

    final product

--- liri.js
+++ liri.js
@@ -8,26 +8,26 @@ var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require("request");
 
-var pick = function(action, search){
+//var pick = function(action, search){
 switch (action) {
   case "my-tweets":
     getMyTweets();
     break;
   
   case "movie-this":
-    getMovieInfo();
+    getMovieInfo(search);
     break;
   
   case "spotify-this-song":
-    getSongInfo();
+    getSongInfo(search);
     break;
   
   case "do-what-it-says":
     getRandom();
     break;
   }
-}
 
+//}
 
 function getMyTweets(){
 
@@ -45,37 +45,45 @@ function getMyTweets(){
   	    	for (var i = 0; i < myTweets.length; i++) {
   		  		console.log(myTweets[i].text)
   		  		console.log(myTweets[i].created_at)
+  		  		console.log("---------------------------------------")
      		}			
   	  	}
   	})
  }
 
-function getSongInfo(){
+function getSongInfo(search){
  	var spotify = new Spotify({
 		id: keys.spotifyKeys.id,
 		secret: keys.spotifyKeys.secret		
 	})
-
+        if (search === undefined) {
+    	search = "The Sign";  
+		}
+  
   spotify
-      .search({ type: 'track', query: search })
+    .search({ type: 'track', query: search })
       .then(function(response){
-       // console.log(typeof response)
+        //console.log(typeof response)
       for(var i in response.tracks.items) {  
         console.log("Artist: " + response.tracks.items[i].artists[0].name)
         console.log("Song Title: " + response.tracks.items[i].name)
         console.log("Album: " + response.tracks.items[i].album.name)
         console.log("Song Preview: " + response.tracks.items[i].preview_url)
+        console.log("---------------------------------------")
       }
+
+  
+
   })
 }
 
 
 function getMovieInfo(){
-  if (search === undefined) {
-    search = "Mr. Nobody";
-  }
-request("http://www.omdbapi.com/?t="+ search +"&y=&plot=short&apikey=40e9cece", function(error, response, body) {  
-  if (!error && response.statusCode === 200) {
+	if (search === undefined) {
+    	search = "Mr Nobody";  
+	}
+	request("http://www.omdbapi.com/?t="+ search +"&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+  	if (!error && response.statusCode === 200) {
     console.log("Title: " + JSON.parse(body).Title);
     console.log("Year Released: " + JSON.parse(body).Year);
     console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
@@ -84,34 +92,29 @@ request("http://www.omdbapi.com/?t="+ search +"&y=&plot=short&apikey=40e9cece", 
     console.log("Language: " + JSON.parse(body).Language)
     console.log("Plot: " + JSON.parse(body).Plot)
     console.log("Actors: " + JSON.parse(body).Actors)
+    console.log("---------------------------------------")
     }
+
   });
 }
  
 
-function getRandom(){
+function getRandom(action, search){
+	if(action === "my-tweets" || "spotify-this-song" + search || "movie-this" + search){
+		fs.readFile("random.txt", "utf8", function(error, data, search) {
+ 		var data
+   		var commands = data.toString().split(",");
+   		
+   		var action = commands[0]
+   		var search = commands[1]
+   		console.log(action + " " + search)
+   		//getMovieInfo(search)
+   		
+   		})
+  	}
+}
 
-	fs.readFile("random.txt", "utf8", function(error, data) {
- 		
-   	var commands = data.split(",");
-   	console.log(data)
-	if (commands.length === 2) {
-      pick(commands[0], commands[1]);
-    }
-    else if (commands.length === 1) {
-      pick(commands[0]);
-    	}
-    })
-  }
-
-
-
-
-
-
-
-
-
+     
 
 
 
